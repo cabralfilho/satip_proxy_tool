61140a96c828782dcc5b3f2012121c2462f3676b
jfont555 <jfont555@gmail.com>
2016-05-17 22:55:35 +0200

    Fix null session  in Options message

--- messages_lib.js
+++ messages_lib.js
@@ -8,7 +8,7 @@ exports.optionsMessage = function(options,cb){
     var msgOut = new String();
     msgOut += "OPTIONS rtsp://"+options.serverAddress+":554/ RTSP/1.0\r\n";
     msgOut += options.nSeq+"\r\n";
-    if(options.session !== undefined){
+    if(options.session !== undefined && options.session !== null){
         msgOut += options.session+"\r\n";
     }
     msgOut += "\r\n";
